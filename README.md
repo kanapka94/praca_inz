18ce9a346c4ed6c20fb86f54a523259dae4d8699
Adam <rumpelteam@gmail.com>
2018-01-02 22:20:02 +0100

    Add gulp default task and scss support

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Praca inżynierska
 
+Kolory: https://www.palettable.io/D43442-FFFFFF-4DBCE9-FFFBB4-B7FFCC
+
+Kolory (wersja mocna): https://www.palettable.io/03031B-FFFFFF-DD5053-6BE067-F5C44B
+
 Autor: Adam Konopka
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
+import { sync } from '../../../../Users/adam/Library/Caches/typescript/2.6/node_modules/@types/glob';
+
 var gulp = require('gulp'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass');
 
 gulp.task('default', ['serve']);
 
-gulp.task('serve', function() {
+gulp.task('serve', ['sass'], function() {
 
     browserSync({
         server: 'app'
     });
 
-    gulp.watch(
-        [
-            'app/*.html',
-            'app/scss/*.scss',
-            'app/js/*.js'
-        ],
-        ['reload']
-    );
+    gulp.watch('app/*.html', ['reload']);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
 
 });
 
 gulp.task('reload', function() {
     browserSync.reload();
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
