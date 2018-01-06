2dd65761a60c4acb1fe5a3daaffdb302c1a176fb
Adam <rumpelteam@gmail.com>
2018-01-06 20:45:52 +0100

    Add jeencrypt library and test pub ras 2048 bit key

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
-import browserDetector from './lib/browserDetect';
+import browserDetector from './lib/browser-detect';
+import cryptoEngine from './lib/upload-page';
 
 document.addEventListener("DOMContentLoaded", function() {
     browserDetector.detect();
+    
+    if ($('.encrypt-form').length) {
+        cryptoEngine.initEngine();
+    }
 });
